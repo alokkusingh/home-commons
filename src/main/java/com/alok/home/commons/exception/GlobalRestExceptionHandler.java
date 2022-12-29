c9de30a634e2d2e49f0bd1f614ccc8f339b7bb26
alokkusingh <alok.ku.singh@gmail.com>
2022-12-29 18:01:47 +0530

    Added ProblemDetails Exception

--- src/main/java/com/alok/home/commons/exception/GlobalRestExceptionHandler.java
+++ src/main/java/com/alok/home/commons/exception/GlobalRestExceptionHandler.java
@@ -7,18 +7,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
-import java.time.Instant;
+import java.time.ZonedDateTime;
 
 @RestControllerAdvice
 public class GlobalRestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    ProblemDetail handleBookmarkNotFoundException(ResourceNotFoundException e) {
+    ProblemDetail handleResourceNotFoundException(ResourceNotFoundException e) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
         problemDetail.setTitle("Resource Not Found");
         problemDetail.setType(URI.create("home-api/errors/not-found"));
         problemDetail.setProperty("errorCategory", "NotFound");
-        problemDetail.setProperty("timestamp", Instant.now());
+        problemDetail.setProperty("timestamp", ZonedDateTime.now());
         return problemDetail;
     }
 }
