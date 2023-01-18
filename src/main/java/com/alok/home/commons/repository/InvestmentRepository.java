87d826fabe024b9b44c09a72b8c4b189716ff0fa
alokkusingh <alok.ku.singh@gmail.com>
2023-01-18 17:36:15 +0530

    Added some security related exceptions

--- src/main/java/com/alok/home/commons/repository/InvestmentRepository.java
+++ src/main/java/com/alok/home/commons/repository/InvestmentRepository.java
@@ -2,7 +2,13 @@ package com.alok.home.commons.repository;
 
 import com.alok.home.commons.model.Investment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface InvestmentRepository extends JpaRepository<Investment, Long> {
+    @Query("SELECT i FROM Investment i WHERE i.yearx = ?1 and i.monthx = ?2")
+    List<Investment> findAllByYearMonth(Integer year, Integer month);
 }
 
